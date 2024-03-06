41fb672233e3897db36996aff305a58bc6f78a43
abdul kabeer khan <162288997+kabeer129@users.noreply.github.com>
2024-03-06 05:27:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var name1 = ("abdul");
+console.log(name1);
+name1 = "kabeer";
+console.log(name1);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let name1 =("abdul")
+console.log(name1);
+
+name1 = "kabeer"
+console.log(name1);
